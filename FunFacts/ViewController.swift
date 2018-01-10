5a030e7de451b4467bf03eed9e932ad9fb30e8c8
Chris Gaona <thebeloved88@sbcglobal.net>
2018-01-10 09:00:19 -0800

    Uses an array to store & access facts

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -14,11 +14,12 @@ class ViewController: UIViewController {
     // they allow us to set values to nil
     // the label may not exist in the UI yet
     @IBOutlet weak var funFactLabel: UILabel!
+    let facts = ["Ants stretch when they wake up in the morning", "Ostriches can run faster than horses"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        funFactLabel.text = "An interesting fact!"
+        funFactLabel.text = facts[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +28,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFact() {
-        funFactLabel.text = "Another interesting fact!"
+        funFactLabel.text = facts[1]
     }
     
 }
